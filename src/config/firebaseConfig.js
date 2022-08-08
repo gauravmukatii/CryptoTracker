2df6a8fc978e5adb1e94471c428259dde71bd61f
Gaurav Mukati <gaurav.mukati99@gmail.com>
2022-08-08 19:21:15 +0530

    Initial commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCz5hVM-krv0zSG20xZJCFSdIsqGRaau8s",
+    authDomain: "crypto-tracker-9c9b9.firebaseapp.com",
+    projectId: "crypto-tracker-9c9b9",
+    storageBucket: "crypto-tracker-9c9b9.appspot.com",
+    messagingSenderId: "520394438613",
+    appId: "1:520394438613:web:7dc4a0af01862945596f5c"
+  };
+
+  export default firebaseConfig;
